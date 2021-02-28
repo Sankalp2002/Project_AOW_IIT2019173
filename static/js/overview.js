4ea031d83fe986bbcc9578c5b30907608ffe7ec5
Ayush Baranwal <baranwalayush25@gmail.com>
2021-02-28 19:10:58 +0530

    Modified the folder for backend

--- static/js/overview.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.clickable').addEventListener('click', function(){
-    var new_div = $('<div>START</div>').addClass('d');    
-  
-    new_div.insertAfter('#carousel-example-generic');  
-});
