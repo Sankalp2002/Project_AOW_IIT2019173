4c02a03644fee94a70fd7a4d618f32ced4dceaa9
Sankalp <53993946+Sankalp2002@users.noreply.github.com>
2021-03-04 02:14:48 +0530

    only frontend

--- frontend/js/overview.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.carousel-caption').addEventListener('click', function(){
-      var new_div = $('<span>START</span>');    
-    
-      new_div.insertAfter('#carousel-example-generic');  
-});
